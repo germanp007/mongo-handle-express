e3d99d08e4124614b53eeddafc85fcaef7c8a6ef
germanp007 <pintogerman281@gmail.com>
2023-09-10 23:07:27 -0300

    second commit

--- src/persistencia/files/cartManager.js
+++ src/persistencia/files/cartManager.js
@@ -4,7 +4,57 @@ export class CartManager {
   constructor(filePath) {
     this.filePath = filePath;
   }
-
-  addProduct() {}
+  fileExists() {
+    return fs.existsSync(this.filePath);
+  }
+  async getCarts() {
+    try {
+      if (fs.existsSync(this.filePath)) {
+        const data = await fs.promises.readFile(this.filePath, "utf-8");
+        const cartsList = JSON.parse(data);
+        console.log(cartsList);
+        return cartsList;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async addCart() {
+    try {
+      if (this.fileExists()) {
+        const carts = await fs.promises.readFile(this.filePath, "utf-8");
+        const cartJson = JSON.parse(carts);
+        const newCart = {
+          id: cartJson.length > 0 ? cartJson[cartJson.length - 1].id + 1 : 1,
+          products: [],
+        };
+        cartJson.push(newCart);
+        await fs.promises.writeFile(
+          this.filePath,
+          JSON.stringify(cartJson, null, 2)
+        );
+      } else {
+        throw new Error("Archivo no existente");
+      }
+    } catch (error) {
+      console.log("Error", error);
+    }
+  }
+  async getCartById(id) {
+    try {
+      if (this.fileExists()) {
+        const carts = await fs.promises.readFile(this.filePath, "utf-8");
+        const cartJson = JSON.parse(carts);
+        const result = cartJson.find((item) => {
+          item.id === id;
+        });
+        return result;
+      } else {
+        throw new Error("No existe el archivo");
+      }
+    } catch (error) {
+      console.log("Error", error);
+    }
+  }
   deleteProduct() {}
 }
--- src/routes/carts.routes.js
+++ src/routes/carts.routes.js
@@ -1,10 +1,32 @@
 import { Router } from "express";
-
+import { cartsManager } from "../persistencia/index.js";
+import express from "express";
 const router = Router();
+router.use(express.json());
+router.use(express.urlencoded({ extended: true }));
 
-let cart = [];
+router.get("/:id", async (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+    const cartById = await cartsManager.getCartById(id);
 
-router.get("/", (req, res) => {
-  res.json({ data: cart });
+    if (cartById) {
+      res.json({ data: cartById });
+    } else {
+      res
+        .status(400)
+        .json({ status: "error", message: "el carrito no existe" });
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+router.post("/", async (req, res) => {
+  try {
+    const cartCreated = await cartsManager.addCart();
+    res.json({ status: "success", data: cartCreated });
+  } catch (error) {
+    res.status(400).json({ status: "error", message: error.message });
+  }
 });
 export { router as routerCart };
