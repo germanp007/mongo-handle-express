25a85452096e6a900baeba0e33c61a733a11a389
German Pinto <93947560+germanp007@users.noreply.github.com>
2023-10-02 18:40:49 -0300

    Update cartManagerMemory.js

--- src/dao/memory/cartManagerMemory.js
+++ src/dao/memory/cartManagerMemory.js
@@ -3,11 +3,27 @@ class CartManagerMemory {
   constructor(){
     this.path = path;
   }
-  async createCart(){
+  async getCarts(){
     try{
-      this.path.readFile()
+      const result = await JSON.parse(this.path.readFile());
+      return result;
     }catch(error){
       console.log('Error al crear el cart')
+    }
+  }
+  async createCarts(body){
+    try{
+      const {title, description, author, date} = body;
+      const data = await JSON.parse(this.path.readFile());
+      const newCart = {
+        id: data.length > 0 ? data[data.legth - 1].id + 1 : 1
+        title,
+        descriptio,
+        author,
+        date
+      };
+    }catch(error){
+      console.log('No fue posible crear el cart')
     }
   }
 
