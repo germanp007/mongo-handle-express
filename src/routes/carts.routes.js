dd7adb01cbc5e09516fb6e21081e21448dc3a757
germanp007 <pintogerman281@gmail.com>
2023-10-04 20:17:46 -0300

    actualizando

--- src/routes/carts.routes.js
+++ src/routes/carts.routes.js
@@ -5,40 +5,16 @@ import express from "express";
 
 const router = Router();
 
-
 router.get("/", async (req, res) => {
   const cartsList = await cartsServices.getCarts();
   res.json({ data: cartsList });
 });
 
-<<<<<<< HEAD
 router.post("/", async (req, res) => {
   try {
     const newCart = {};
     const cartCreated = await cartsServices.createCart(newCart);
 
-=======
-router.get("/:cartId", async (req, res) => {
-  try {
-    const cartId = req.params.cartId;
-    const cartById = await cartsServices.getCartById(cartId);
-
-    if (cartById) {
-      res.json({ data: cartById });
-    } else {
-      res
-        .status(400)
-        .json({ status: "error", message: "el carrito no existe" });
-    }
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
-
-router.post("/", async (req, res) => {
-  try {
-    const cartCreated = await cartsServices.createCart();
->>>>>>> 6bd08a9b57f3c532f2e37d95e32079dd8edb8186
     res.json({ status: "success", data: cartCreated });
   } catch (error) {
     res.status(400).json({ status: "error", message: error.message });
