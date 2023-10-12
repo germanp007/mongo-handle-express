4f42d51513bf19e9cc63601d0cb6879cf5786c64
German Pinto <93947560+germanp007@users.noreply.github.com>
2023-10-12 14:30:31 -0300

    Update products.js

--- src/public/js/products.js
+++ src/public/js/products.js
@@ -1,5 +1,6 @@
-const cart = document.getElementById("go-to-cart");
-
-const addToCart = (productId) => {
-  console.log(productId);
-};
+function changeLimit() {
+  const limit = document.getElementById("limit").value;
+ 
+  window.location.href = `/products?limit=${limit}`;
+  
+}
