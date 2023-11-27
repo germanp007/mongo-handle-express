53e628352763e9447918ab64a1528b25ac787640
germanp007 <pintogerman281@gmail.com>
2023-11-27 20:35:52 -0300

    Terminando el challenge

--- src/controller/carts.controller.js
+++ src/controller/carts.controller.js
@@ -1,5 +1,7 @@
 import { CartsService } from "../service/carts.service.js";
-
+import { ProductsService } from "../service/products.service.js";
+import { v4 as uuidv4 } from "uuid";
+import { TicketsService } from "../service/ticket.service.js";
 export class CartsController {
   static getCarts = async (req, res) => {
     const cartsList = await CartsService.getCarts();
@@ -74,6 +76,41 @@ export class CartsController {
     try {
       const { cid: cartId } = req.params;
       const isCart = await CartsService.getCartById(cartId);
+      //verificar el Stock de cada Producto
+
+      if (isCart.products.length) {
+        const ticketProduct = [];
+        const rejectedProduct = [];
+        for (let i = 0; i < isCart.products.length; i++) {
+          // Bucle q funciona async
+          const cartProduct = isCart.products[i];
+          const productInfo = cartProduct.productId;
+          console.log(productInfo);
+          //Revisar cantidad de stock en Products List y comparar con cantidad en Cart
+          if (cartProduct.quantity <= productInfo.stock) {
+            ticketProduct.push(cartProduct);
+          } else {
+            rejectedProduct.push(cartProduct);
+          }
+        }
+      }
+      const newTicket = {
+        code: uuidv4(),
+        purchase_datetime: new Date(),
+        amount: ticketProduct.reduce((acc, product) => acc + product.price, 0),
+        purchaser: req.user.email,
+      };
+      console.log(newTicket);
+      await TicketsService.createTicket(newTicket);
+      //actualizar carrito con rejected products
+      if (rejectedProduct.length) {
+        res.json({
+          message: "Estos productos no cumplen con el stock",
+          data: rejectedProduct,
+        });
+      } else {
+        res.json({ message: "Compra realizada con exito" });
+      }
     } catch (error) {
       res.json({ status: "error", message: error.message });
     }
