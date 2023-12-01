85de1983e64c619af7ddf80f0c27212df73dbeb7
germanp007 <pintogerman281@gmail.com>
2023-12-01 12:17:18 -0300

    creando el manejador de errores

--- src/helpers/mock.js
+++ src/helpers/mock.js
@@ -1,11 +1,17 @@
 import { faker } from "@faker-js/faker";
 
+const codeGenerate = () => {
+  const letters = faker.random.alpha({ count: 3 });
+  const numbers = faker.random.numeric(3);
+  return `${letters}${numbers}`.toUpperCase();
+};
+
 export const generateFakeProducts = () => {
   return {
     id: faker.database.mongodbObjectId(),
     title: faker.commerce.productName(),
     description: faker.commerce.productDescription(),
-    code: faker.random.alpha(6),
+    code: codeGenerate(),
     price: parseInt(faker.commerce.price()),
     thumbnails: faker.image.imageUrl(),
     stock: parseInt(faker.random.numeric()),
