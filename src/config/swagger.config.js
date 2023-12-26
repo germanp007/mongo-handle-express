1e41e8924403eca986536187048b5a7b12163ea2
germanp007 <pintogerman281@gmail.com>
2023-12-26 10:37:57 -0300

    creando doc de la API

---
+++ src/config/swagger.config.js
@@ -0,0 +1,18 @@
+import { __dirname } from "../utils.js";
+import swaggerJsDoc from "swagger-jsdoc";
+
+import path from "path";
+
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.1",
+    info: {
+      title: "Documentacion api Ecommerce",
+      version: "1.0.0",
+      description: "Definición de endpoints para la API de Ecommerce",
+    },
+  },
+  apis: [`${path.join(__dirname, "../docs/**/*.yaml")}`],
+};
+
+export const swaggerSpecs = swaggerJsDoc(swaggerOptions);
